eaa01a2140425c846bfb9c23e5e6b553715facac
Allan Vikiru <42599978+AllanVikiru@users.noreply.github.com>
2019-03-12 17:49:53 +0300

    Update updateUserDetails.php

--- ajax/updateUserDetails.php
+++ ajax/updateUserDetails.php
@@ -19,16 +19,4 @@ if(isset($_POST['user_id']) && isset($_POST['user_id']) != "")
         exit(mysqli_error($con));
     }
 }
-
-if(isset($_POST['user_id']) && isset($_POST['user_id']) != "")
-{
-    $userID = $_POST['user_id'];
-    $status = 'Resident';
-   
-    // Update User details
-    $query1 = "UPDATE users SET username = '$username', houseNumber = '$houseNumber',  Position = '$status',  email = '$email', phoneNo = '$phoneNo' WHERE user_id = '$userID'";
-    if (!$result = mysqli_query($con, $query1)) {
-        exit(mysqli_error($con));
-    }
-}
 ?>
